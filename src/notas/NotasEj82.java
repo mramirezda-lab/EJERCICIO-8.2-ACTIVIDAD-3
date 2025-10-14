04debc9c7fbe7cfe05de387eea052929e5e920ef
Migue <Migue@LAPTOP-H5CIMOMH>
2025-10-14 00:07:23 -0500

    commit

---
+++ src/notas/NotasEj82.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package notas;
+
+/**
+ *
+ * @author Migue
+ */
+public class NotasEj82 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal formulario = new VentanaPrincipal();
+        formulario.setResizable(false);
+        formulario.setVisible(true);
+    }
+    
+}
